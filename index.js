7307c844e278ea73b8f7e938ba80340ca04a9a14
hvlopez <hectorvmlopez@gmail.com>
2022-08-02 11:52:45 -0500

    adding db connections

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+import dotenv from "dotenv";
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
+const connect = () => {
+  const key = process.env.MONGO;
+  mongoose
+    .connect(key)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
+};
 
 app.listen(8800, () => {
+  connect();
   console.log("Server is running on port 8800");
 });
