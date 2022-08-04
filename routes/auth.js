23cecf54502aa0b630b78356082910090ad89a54
hvlopez <hectorvmlopez@gmail.com>
2022-08-04 14:42:31 -0500

    updating repo

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signin, signup } from "../controllers/auth.js";
+import { googleAuth, signin, signup } from "../controllers/auth.js";
 
 const router = Router();
 
@@ -10,6 +10,6 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 
 // //GOOGLE AUTHENTICATION
-// router.post("/google", signup);
+router.post("/google", googleAuth);
 
 export default router;
